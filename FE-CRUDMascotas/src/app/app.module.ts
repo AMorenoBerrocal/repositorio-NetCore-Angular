55f402b9aba4df9e8bde8f3020302dd88a43cdaf
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-22 11:42:09 +0200

    Creacion del proyecto y configuracion angular material

---
+++ FE-CRUDMascotas/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// Componentes
+import { AgregarEditarMascotaComponent } from './components/agregar-editar-mascota/agregar-editar-mascota.component';
+import { ListadoMascotaComponent } from './components/listado-mascota/listado-mascota.component';
+import { VerMascotaComponent } from './components/ver-mascota/ver-mascota.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// Angular material
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AgregarEditarMascotaComponent,
+    ListadoMascotaComponent,
+    VerMascotaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
