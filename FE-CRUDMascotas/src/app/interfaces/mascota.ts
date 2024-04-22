1422f6b4fc4331c42f69514402521d9196361878
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-22 11:03:50 +0200

    Creacion del proyecto y configuracion angular

---
+++ FE-CRUDMascotas/src/app/interfaces/mascota.ts
@@ -0,0 +1,8 @@
+export interface Mascota {
+    id?: number,            // El id se va a crear automaticamente
+    nombre: string,
+    edad: number,
+    raza: string,
+    color: string,
+    peso: number
+}
