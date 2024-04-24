ea89cf1f226e8e9c6a024c9859eb2c6e6abed0d9
AVANADE-CORP\a.moreno.berrocal <a.moreno.berrocal@avanade.com>
2024-04-24 11:09:26 +0200

    show pet

--- FE-CRUDMascotas/src/app/components/ver-mascota/ver-mascota.component.ts
+++ FE-CRUDMascotas/src/app/components/ver-mascota/ver-mascota.component.ts
@@ -1,15 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+import { Mascota } from 'src/app/interfaces/mascota';
+import { MascotaService } from 'src/app/services/mascota.service';
 
 @Component({
   selector: 'app-ver-mascota',
   templateUrl: './ver-mascota.component.html',
   styleUrls: ['./ver-mascota.component.css']
 })
-export class VerMascotaComponent implements OnInit {
+export class VerMascotaComponent implements OnInit, OnDestroy {
+  id!: number;
+  mascota!: Mascota;
+  loading: boolean = false;
 
-  constructor() { }
+  routeSub!: Subscription;
+
+  // mascota$!: Observable<Mascota>   PIP Async
+
+  constructor(private _mascotaService:MascotaService, 
+    private aRoute: ActivatedRoute) {
+      // Obtenemos el id de la mascota: opcion para obtener el id por ruta
+      // Problema: casos de uso donde no se actualice correctamente
+      this.id = parseInt(this.aRoute.snapshot.paramMap.get('id')!);
+  }
 
   ngOnInit(): void {
+    // this.mascota$ = this._mascotaService.getMascota(this.id); // PIP Async
+    // Ruteo:
+    /*this.routeSub = this.aRoute.params.subscribe(data => {
+      this.id = data['id'];
+      this.obtenerMascota();
+    })*/
+    this.obtenerMascota();
+  }
+
+  ngOnDestroy(): void {
+    /*this.routeSub.unsubscribe();*/
+  }
+
+  obtenerMascota() {
+    this.loading=true;
+    this._mascotaService.getMascota(this.id).subscribe( data => {
+        this.mascota = data;
+        this.loading =false; 
+      }
+    )
   }
 
 }
